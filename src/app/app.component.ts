59c47277454f8c45b513e0560c32c791c89edb8c
André Carreira <andre_tsc_@hotmail.com>
2025-10-15 14:05:44 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rocketlan';
+  onRegister() {
+    window.open(
+      'https://docs.google.com/forms/d/e/1FAIpQLSfhHQwHSnbbFxLvfhREN_uwA0kj4A8rWd7QCdwm_vjhzMnU_g/viewform?usp=dialog',
+      '_blank'
+    );
+  }
+
+  onInfo() {
+    alert('Mais informações em breve!');
+  }
+
+  onDiscord() {
+    window.open('https://discord.gg/seu-link-aqui', '_blank');
+  }
 }
